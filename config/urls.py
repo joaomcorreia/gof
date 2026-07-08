2e4d32c8ec94c7d4d5cf92c9b99756d007ef9d62
joaomcorreia <joao@justcodeworks.com>
2026-07-09 00:12:57 +0200

    Implement public language entry redirects

--- config/urls.py
+++ config/urls.py
@@ -10,12 +10,16 @@ urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('robots.txt', core_views.robots_txt, name='robots_txt'),
     path('sitemap.xml', core_views.sitemap_xml, name='sitemap_xml'),
-    path('help/', RedirectView.as_view(pattern_name='content:help_index', permanent=False)),
+    path('', core_views.redirect_public_entry_to_language, {'url_name': 'core:home'}),
+    path('start/', core_views.redirect_public_entry_to_language, {'url_name': 'ai_starter:start'}),
+    path('pricing/', core_views.redirect_public_entry_to_language, {'url_name': 'core:pricing'}),
+    path('contact/', core_views.redirect_public_entry_to_language, {'url_name': 'core:contact'}),
+    path('staff/', core_views.unprefixed_staff_route_not_found),
+    path('help/', core_views.redirect_public_entry_to_language, {'url_name': 'content:help_index'}),
+    path('blog/', core_views.redirect_public_entry_to_language, {'url_name': 'blog:index'}),
     path('help/<slug:slug>/', RedirectView.as_view(pattern_name='content:help_detail', permanent=False)),
-    path('blog/', RedirectView.as_view(pattern_name='blog:index', permanent=False)),
     path('blog/category/<slug:slug>/', RedirectView.as_view(pattern_name='blog:category', permanent=False)),
     path('blog/<slug:slug>/', RedirectView.as_view(pattern_name='blog:detail', permanent=False)),
-    path('', RedirectView.as_view(pattern_name='core:home', permanent=False)),
 ]
 
 urlpatterns += i18n_patterns(
